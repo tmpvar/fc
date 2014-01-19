f4f25eccf37368359d5070218c939a2eca9cdfdd
Elijah Insua <tmpvar@gmail.com>
2014-01-19 00:49:34 -0700

    baseline implementation of fullscreen canvas

--- fc.js
+++ fc.js
@@ -1,3 +1,58 @@
-module.exports = function() {
+// Fullscreen canvas
+function fc(fn, autorun) {
+  document.body.style.margin = "0px";
+  document.body.style.padding = "0px";
 
+  var canvas = document.createElement('canvas');
+  document.body.appendChild(canvas);
+  var ctx = canvas.getContext('2d');
+
+  var last = 0, dirty = false, request;
+  
+  function tick(time) {
+    var delta = time-last;
+    last = time;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.fillStyle = "#223";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.save();
+    fn && fn(delta);
+    ctx.restore();
+    if (autorun !== false) {
+      requestAnimationFrame(tick);
+    }
+    dirty = false;
+  }
+
+  autorun !== false && requestAnimationFrame(tick);
+
+  ctx.dirty = function() {
+    if (!dirty) {
+      request = requestAnimationFrame(tick);
+      dirty = true;
+    }
+  };
+
+  ctx.stop = function() {
+    autorun = false;
+    dirty = false;
+    request && cancelAnimcationFrame(request);
+  };
+
+  ctx.start = function() {
+    autorun = true;
+    requestAnimationFrame(tick);
+  };
+
+  ctx.canvas = canvas;
+  return ctx;
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = fc;
+}
+
+if (typeof window !== 'undefined') {
+  window.fc = window.fc || fc;
 }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,88 @@
-var fc = require('../fc.js');
-var assert = require('assert');
+
+if (typeof require === 'function') {
+  var fc = require('../fc.js');
+} else {
+
+}
+
+var ok = function(a) {
+  if (!a) {
+    throw new Error('not ok')
+  }
+};
 
 describe('fc', function() {
-  it('', function() {
-    
+  it('should create a canvas element on the body', function() {
+    var ctx = fc(null, false);
+    ok(document.body.lastChild.tagName.toLowerCase() === 'canvas');
+  });
+
+  it('should hang the canvas off of ctx', function() {
+    var ctx = fc(null, false);
+    ok(document.body.lastChild === ctx.canvas);
+  });
+
+  it('should callback with deltatime', function(t) {
+    var ctx = fc(function(delta) {
+      delta = parseFloat(delta);
+      ok(!isNaN(delta));
+      ok(delta>0);
+      t();
+    }, false);
+    ctx.dirty();
+  });
+
+  describe('autorun', function() {
+    it('should add a stop method to the ctx', function(t) {
+      var val = 0
+
+      var ctx = fc(function() {
+        val++;
+        ctx.stop(); 
+        setTimeout(function() {
+          ok(val === 1);
+          t();
+        }, 30);
+      });
+    });
+
+    it('should add a start method to the ctx', function(t) {
+      var val = 0
+
+      var ctx = fc(function() {
+        console.log('here?')
+        val++;
+        ctx.stop(); 
+        setTimeout(function() {
+          ok(val === 1);
+          t();
+        }, 30);
+      }, false);
+
+      setTimeout(function() {
+        ok(val === 0);
+        ctx.start();
+      }, 30);
+
+    });
+  });
+
+  describe('render when dirty', function(t) {
+    it('should only request one frame per cycle', function(t) {
+      var val = 0
+
+      var ctx = fc(function() {
+        val++;
+      }, false);
+
+      ctx.dirty();
+      ctx.dirty();
+      ctx.dirty();
+      ctx.dirty();
+      setTimeout(function() {
+        ok(val === 1);
+        t();
+      }, 40);
+    });
   });
 });
